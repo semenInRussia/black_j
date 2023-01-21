d8e77495a30d1e77a55ac9186c7713e880f30fa3
semenInRussia <hrams205@gmail.com>
2023-01-21 20:47:02 +0300

    some small upgrades for "branch.py"

--- black_j/branch.py
+++ black_j/branch.py
@@ -12,18 +12,20 @@ from .hand import is_hand_lose
 T = TypeVar("T")
 Branch = tuple[str, list[Hand]]
 
+
 def branches_for_some_hands(hands: list[Hand]) -> list[Branch]:
-    if len(hands) == 1:
-        hand = hands[0]
-    else:
-        hand = select_one_hand(hands)
+    if not hands:
+        return []
+
+    hand = select_one_hand(hands)
     hands.remove(hand)
+
     hand_branches = branches_for_one_hand(hand)
     branches = []
 
     for hand_branch in hand_branches:
         branch_name, branch_hands = hand_branch
-        branches.append((branch_name, branch_hands + hands))
+        branches += [(branch_name, branch_hands + hands)]
 
     return branches
 
@@ -49,18 +51,20 @@ def select_branch(branches: list[Branch]) -> Branch:
     i = 1
     for branch in branches:
         print(f" {i}. ", end=" ")
-        print(branch[0])        # name
+        print(branch[0])
         i +=  1
 
     print(" --- ")
     q = input("Что делать? ")
 
+
     if q == "win":
         print("Уго, поздравляю, вы выиграли!")
+        exit()
 
     action_n = int(q)
 
     while not 0 < action_n <= len(branches):
-        action_n = int(input("Что делать? "))
+        action_n = int(input("Чтоо делать? "))
 
     return branches[action_n - 1]
